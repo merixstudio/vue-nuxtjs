a59db73ca931470d5a2c141afab29e0272d8b3ea
Amadeusz Kozłowski <a.kozlowski@merixstudio.com>
2019-10-21 15:04:35 +0200

    homepage search

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-content>
+    <v-container
+      class="fill-height"
+      fluid
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          cols="12"
+          sm="8"
+          md="4"
+        >
+          <v-card class="elevation-12">
+            <v-toolbar
+              color="warning"
+              dark
+              flat
+            >
+              <v-toolbar-title>Find beer for Your meal!</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="Search | eg. chicken, pasta..."
+                  name="search"
+                  type="text"
+                  v-model="search"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn nuxt=true :to="`/list/${search}`" color="warning">Search</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      search: ''
+    }
+  }
+}
+</script>
